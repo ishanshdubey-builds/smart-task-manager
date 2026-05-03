8f973128f60403be2f3c7718d32712558c148cab
Ishansh Dubey <ishanshdubey0@gmail.com>
2026-05-04 01:32:28 +0530

    Fix CORS preflight FINAL

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,11 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://smart-task-manager-drab.vercel.app")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+  
+   if (req.method === "OPTIONS") {
+    return res.sendStatus(200)
+  }
+
   next()
 })
 
